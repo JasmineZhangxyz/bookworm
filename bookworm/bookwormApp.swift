854f90f05631bba1e0785259f0f66354de48fead
Xiyue Zhang <xiyuej.zhang@mail.utoronto.ca>
2023-07-23 14:50:04 -0400

    Initial Commit

---
+++ bookworm/bookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookwormApp.swift
+//  bookworm
+//
+//  Created by Jasmine Zhang on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct bookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
